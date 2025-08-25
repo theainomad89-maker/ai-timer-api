fcafde67e00de1c460c5a9632ce1b7e1b7077a11
Alex Rieger <riegeraw@gmail.com>
2025-08-25 15:20:54 +0700

    Fix Fastify CORS implementation

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 import Fastify from "fastify";
-import cors from "cors";
 import OpenAI from "openai";
 
 const app = Fastify();
-// CORS (simple)
-// @ts-ignore
-app.use(cors());
+
+// Enable CORS
+app.addHook('onRequest', (request, reply, done) => {
+  reply.header('Access-Control-Allow-Origin', '*');
+  reply.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  reply.header('Access-Control-Allow-Headers', 'Content-Type');
+  
+  if (request.method === 'OPTIONS') {
+    reply.send();
+    return;
+  }
+  done();
+});
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
